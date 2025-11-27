231ef039f439c09d0e2aaff8e6a9df45812a81a9
jobin jose samuel <jobin.samuel@outlook.com>
2025-11-27 21:54:38 +0530

    category CRUD

---
+++ YumBlazor/Helper/IJSRuntimeExtentions.cs
@@ -0,0 +1,23 @@
+﻿﻿using Microsoft.JSInterop;
+
+namespace LearnBlazor.Helper
+{
+    public static class IJSRuntimeExtentions
+    {
+        public static async Task<bool> ConfirmBox(this IJSRuntime jsRuntime, string message)
+        {
+            return await jsRuntime.InvokeAsync<bool>("confirm", message);
+        }
+
+        //----Toastr---
+        public static async Task ToastrSuccess(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("showToastr", "success", message);
+        }
+
+        public static async Task ToastrError(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("showToastr", "error", message);
+        }
+    }
+}
---
+++ YumBlazor/wwwroot/js/common.js
@@ -0,0 +1,20 @@
+﻿window.showToastr = function (type, message) {
+
+    if (type == "success") {
+        toastr.success(message);
+    }
+
+    if (type == "error") {
+        toastr.error(message);
+    }
+}
+
+function ShowConfirmationModal(){
+
+    bootstrap.Modal.getOrCreateInstance(document.getElementById('bsConfirmationModal')).show();
+}
+
+function HideConfirmationModal(){
+
+    bootstrap.Modal.getOrCreateInstance(document.getElementById('bsConfirmationModal')).hide();
+}
